4b5779feb6e80f73fcc585ac398866f21b968b7b
Daniel Gill <dwgill@outlook.com>
2017-04-20 12:17:47 -0400

    Initial commit w/pelican

---
+++ content/pages/about.md
@@ -0,0 +1,12 @@
+title: About
+Date: 2017-04-20 12:00
+Modified: 2017-04-20 12:00
+Category: etc.
+Tags: meta
+Slug: about
+Authors: Daniel Gill
+Summary: Site introduction & about
+
+Hello, I'm Daniel Gill. I like reading, learning, and occasionally
+thinking. I'm a coder and a tabletop role-playing game enthusiast, and
+I'll be using this blog to write about either topic from time to time.
---
+++ pelicanconf.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Daniel Gill'
+SITENAME = 'Tetrahedrons & Terminals'
+SITEURL = ''
+
+THEME = 'built-texts'
+
+PATH = 'content'
+
+TIMEZONE = 'America/Detroit'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('Lit & News Tumblr', 'https://dwgill.tumblr.com'),
+          ('Twitter', 'https://twitter.com/danwgill'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
